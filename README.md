cc34d8fa60ee3696129220b93e8574cd8d3f7ea0
Mars <marianomarquis@gmail.com>
2015-08-10 11:17:20 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# DisGistsList
+# DisGistsList
---
+++ public/gistlist.php
@@ -0,0 +1,16 @@
+<?php
+
+ ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <h1 style="centered ">GistList</h1>
+    <a href="">US Canada Europe Asia/Pacific/Middle East Oceania Latin America Africa</a>
+
+  </body>
+</html>
