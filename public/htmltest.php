65b88838a27f62f6f1ae3c255626f2779856b558
Lorne <lorne21@gmail.com>
2015-08-14 10:28:08 -0500

    finalized user create

--- public/htmltest.php
+++ public/htmltest.php
@@ -1,8 +1,11 @@
 <?php
+
 require_once "ad.show.php";
+require_once "users.create.php";
 
 
 ?>
+
 <html>
 <head>
     <title>Batlist</title>
@@ -63,7 +66,7 @@ require_once "ad.show.php";
                         <a class="page-scroll" href="#post_ad">List your Bat Ad</a>
                     </li>
                     <li>
-                        <a class="page-scroll" href="#signup">Sign Up</a>
+                        <a href="#" data-toggle="modal" data-target="#registerModal">Sign Up</a>
                     </li>
                     <li>
                         <a class="page-scroll" href="#login">Log In</a>
@@ -132,6 +135,48 @@ require_once "ad.show.php";
             </div>
         </div>
     </section>
+
+    <!-- REGISTER MODAL -->
+    <div class="modal fade" id="registerModal" tabindex="-1" role="dialog" aria-labelledby="registerModalLabel">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="registerModalLabel">Please create a new alter ego</h4>
+          </div>
+          <div class="modal-body">
+            <form method="POST" action="htmltest.php">
+                  <div class="userCreate form-group">
+                      <label for="first_name"></label>
+                      <input type="text" name="first_name" class="form-control" id="first_name" placeholder="First Name">
+                  </div>
+                  <div class="userCreate form-group">
+                      <label for="last_name"></label>
+                      <input type="text" name="last_name" class="form-control" id="last_name" placeholder="Last Name">
+                  </div>
+                  <div class="userCreate form-group">
+                      <label for="user_name"></label>
+                      <input type="text" name="user_name" class="form-control" id="user_name" placeholder="Username">
+                  </div>
+                  <div class="userCreate form-group">
+                      <label for="password"></label>
+                      <input type="text" name="password" class="form-control" id="password" placeholder="Password">
+                  </div>
+                  <div class="userCreate form-group">
+                      <label for="email"></label>
+                      <input type="text" name="email" class="form-control" id="email" placeholder="Email">
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-warning">Submit</button>
+                  </div>
+                  
+              </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- END REGISTER MODAL -->
 
     <footer>
         <div class="container text-center">
